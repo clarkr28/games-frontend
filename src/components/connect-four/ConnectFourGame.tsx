8fe7a4943120187e67824000ac84547640a11d53
Jared Clark <rjaredclark@gmail.com>
2023-01-14 23:05:57 -0600

    added reset button

--- src/components/connect-four/ConnectFourGame.tsx
+++ src/components/connect-four/ConnectFourGame.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { ConnectFourBoard } from "./ConnectFourBoard";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+    reset,
+    selectStatus,
+} from "../../features/connect-four/connectFourSlice";
+import { C4GameStatus } from "../../assets/ConnectFourResources";
 
 export const ConnectFourGame: React.FC<{}> = (props) => {
-    return <ConnectFourBoard />;
+    const gameStatus = useAppSelector(selectStatus);
+    const dispatch = useAppDispatch();
+    return (
+        <>
+            <ConnectFourBoard />
+            <button
+                onClick={() => dispatch(reset())}
+                disabled={gameStatus === C4GameStatus.New}
+            >
+                reset
+            </button>
+        </>
+    );
 };
