c4280200c92c6779e554cc0c7c7ec0fbcbcd532e
Jared Clark <rjaredclark@gmail.com>
2023-03-25 12:32:13 -0400

    added homepage card for game of life

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,33 +6,6 @@ import "./HomePageStyles.css";
 import { Suit, suitToUnicode } from "../assets/CardResources";
 
 export const HomePage: React.FC<{}> = () => {
-    const ticTacToeDisplay = (
-        <div className="fourIconGrid">
-            <FontAwesomeIcon icon={solid("x")} />
-            <FontAwesomeIcon icon={solid("o")} />
-            <FontAwesomeIcon icon={solid("o")} />
-            <FontAwesomeIcon icon={solid("x")} />
-        </div>
-    );
-
-    const connectFourDisplay = (
-        <div className="fourIconGrid">
-            <FontAwesomeIcon icon={solid("circle")} />
-            <FontAwesomeIcon className="red" icon={solid("circle")} />
-            <FontAwesomeIcon className="red" icon={solid("circle")} />
-            <FontAwesomeIcon icon={solid("circle")} />
-        </div>
-    );
-
-    const blackjackDisplay = (
-        <div className="fourIconGrid">
-            <div>{suitToUnicode(Suit.Spade)}</div>
-            <div className="red">{suitToUnicode(Suit.Heart)}</div>
-            <div className="red">{suitToUnicode(Suit.Diamond)}</div>
-            <div>{suitToUnicode(Suit.Club)}</div>
-        </div>
-    );
-
     return (
         <div className="homePageCardContainer">
             <GameCard
@@ -50,6 +23,115 @@ export const HomePage: React.FC<{}> = () => {
                 route="blackjack"
                 display={blackjackDisplay}
             />
+            <GameCard
+                buttonLabel="Play Life"
+                route="life"
+                display={lifeDisplay}
+            />
         </div>
     );
 };
+
+const ticTacToeDisplay = (
+    <div className="fourIconGrid">
+        <FontAwesomeIcon icon={solid("x")} />
+        <FontAwesomeIcon icon={solid("o")} />
+        <FontAwesomeIcon icon={solid("o")} />
+        <FontAwesomeIcon icon={solid("x")} />
+    </div>
+);
+
+const connectFourDisplay = (
+    <div className="fourIconGrid">
+        <FontAwesomeIcon icon={solid("circle")} />
+        <FontAwesomeIcon className="red" icon={solid("circle")} />
+        <FontAwesomeIcon className="red" icon={solid("circle")} />
+        <FontAwesomeIcon icon={solid("circle")} />
+    </div>
+);
+
+const blackjackDisplay = (
+    <div className="fourIconGrid">
+        <div>{suitToUnicode(Suit.Spade)}</div>
+        <div className="red">{suitToUnicode(Suit.Heart)}</div>
+        <div className="red">{suitToUnicode(Suit.Diamond)}</div>
+        <div>{suitToUnicode(Suit.Club)}</div>
+    </div>
+);
+
+const lifeDisplay = (
+    <div className="fourIconGrid">
+        <div className="lifeDisplayGrid">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="lifeDisplayGrid">
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+        </div>
+        <div className="lifeDisplayGrid">
+            <div></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+        </div>
+        <div className="lifeDisplayGrid">
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div className="blackBackground"></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+);
