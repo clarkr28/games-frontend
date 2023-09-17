d94990c522fa13341692fc1fa15dec703f5b0e40
Jared Clark <rjaredclark@gmail.com>
2023-09-17 13:07:33 -0400

    reset meeple count

--- src/assets/avila/Resources.ts
+++ src/assets/avila/Resources.ts
@@ -574,7 +574,7 @@ export enum AvilaPlayerColor {
 export function createPlayer(color: AvilaPlayerColor): IAvilaPlayer {
     return {
         score: 0,
-        availableMeeple: 2,
+        availableMeeple: 6,
         color: color,
     };
 }
