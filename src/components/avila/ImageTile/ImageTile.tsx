18188c39c37aaf3df49d3284e6ce173432473fcb
Jared Clark <rjaredclark@gmail.com>
2023-11-01 21:27:08 -0400

    placed meeples display on image tiles

--- src/components/avila/ImageTile/ImageTile.tsx
+++ src/components/avila/ImageTile/ImageTile.tsx
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AvilaPlayerColor, IAvilaTile } from "../../../assets/avila/Resources";
 import { IconTile } from "../IconTile/IconTile";
 import styles from "./ImageTile.module.scss";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export interface IImageTileProps {
     tile: IAvilaTile;
@@ -13,12 +15,63 @@ export const ImageTile: React.FC<IImageTileProps> = (props) => {
         return <IconTile tile={tile} placeMeepleColor={placeMeepleColor} />;
     }
 
+    const Meeple = (
+        <div
+            className={`${styles.placedMeeple} ${meeplePlacement(
+                tile
+            )} ${meepleColor(tile)}`}
+        >
+            <FontAwesomeIcon icon={solid("person")} />
+        </div>
+    );
+
     return (
-        <img
-            className={styles.imageTile}
-            style={{ transform: `rotate(${tile.rotation}deg)` }}
-            src={tile.imageFile}
-            alt="image tile"
-        />
+        <div className={styles.wrapper}>
+            <img
+                className={styles.imageTile}
+                style={{ transform: `rotate(${tile.rotation}deg)` }}
+                src={tile.imageFile}
+                alt="image tile"
+            />
+            {tile.meeple && Meeple}
+        </div>
     );
 };
+
+function meeplePlacement(tile: IAvilaTile): string {
+    if (!tile.meeple) {
+        return "";
+    }
+    if (tile.meeple.onMonestary) {
+        return styles.monestaryMeeple;
+    }
+    switch (tile.meeple.edgeIndex) {
+        case 0:
+            return styles.topMeeple;
+        case 1:
+            return styles.rightMeeple;
+        case 2:
+            return styles.bottomMeeple;
+        case 3:
+            return styles.leftMeeple;
+    }
+    return "";
+}
+
+function meepleColor(tile: IAvilaTile): string {
+    if (!tile.meeple) {
+        return "";
+    }
+    switch (tile.meeple.playerColor) {
+        case AvilaPlayerColor.Blue:
+            return styles.blue;
+        case AvilaPlayerColor.Green:
+            return styles.green;
+        case AvilaPlayerColor.Orange:
+            return styles.orange;
+        case AvilaPlayerColor.Purple:
+            return styles.purple;
+        case AvilaPlayerColor.Red:
+            return styles.red;
+    }
+}
--- src/components/avila/TileTester/TileTester.tsx
+++ src/components/avila/TileTester/TileTester.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { IAvilaTile } from "../../../assets/avila/Resources";
+import { AvilaPlayerColor, IAvilaTile } from "../../../assets/avila/Resources";
 import { createTiles } from "../../../assets/avila/TileResources";
 import { AvilaGrid } from "../AvilaGrid/AvilaGrid";
 
@@ -9,6 +9,7 @@ export const TileTester: React.FC<{}> = () => {
     // get the tiles and turn them into a grid
     useEffect(() => {
         const tiles = createTiles(false);
+        customizeTiles(tiles);
         let row: IAvilaTile[] = [];
         const tempGrid: IAvilaTile[][] = [];
         tiles.forEach((tile, index) => {
@@ -32,3 +33,31 @@ export const TileTester: React.FC<{}> = () => {
         </div>
     );
 };
+
+function customizeTiles(tiles: IAvilaTile[]): void {
+    tiles[40].meeple = {
+        playerColor: AvilaPlayerColor.Blue,
+        playerIndex: 0, // doesn't matter for this test
+        edgeIndex: 3,
+    };
+    tiles[16].meeple = {
+        playerColor: AvilaPlayerColor.Green,
+        playerIndex: 0, // doesn't matter for this test
+        edgeIndex: 0,
+    };
+    tiles[17].meeple = {
+        playerColor: AvilaPlayerColor.Orange,
+        playerIndex: 0, // doesn't matter for this test
+        edgeIndex: 1,
+    };
+    tiles[41].meeple = {
+        playerColor: AvilaPlayerColor.Purple,
+        playerIndex: 0, // doesn't matter for this test
+        edgeIndex: 2,
+    };
+    tiles[45].meeple = {
+        playerColor: AvilaPlayerColor.Red,
+        playerIndex: 0, // doesn't matter for this test
+        onMonestary: true,
+    };
+}
