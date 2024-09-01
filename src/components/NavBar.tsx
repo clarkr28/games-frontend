23ec0911ae846f4ab773b528ae6f50c75feeb7cd
Jared Clark <rjaredclark@gmail.com>
2024-08-31 23:47:36 -0400

    made top nav prettier

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavBarStyles.css";
 
 export const NavBar: React.FC<{}> = () => {
-    return (
-        <nav>
-            <Link to="/">Home</Link>
-            {process.env.REACT_APP_ENABLE_TICTACTOE && (
-                <Link to="/tic-tac-toe">Tic Tac Toe</Link>
-            )}
-            {process.env.REACT_APP_ENABLE_CONNECTFOUR && (
-                <Link to="/connect-four">Connect Four</Link>
-            )}
-            {process.env.REACT_APP_ENABLE_BLACKJACK && (
-                <Link to="/blackjack">Blackjack</Link>
-            )}
-            {process.env.REACT_APP_ENABLE_LIFE && <Link to="/life">Life</Link>}
-            {process.env.REACT_APP_ENABLE_AVILA && (
-                <Link to="/avila">Avila</Link>
-            )}
-            {process.env.REACT_APP_AVILA_TILE_TEST && (
-                <Link to="/avila-tile-test">Tile Tester</Link>
-            )}
-        </nav>
-    );
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <div>
+        {process.env.REACT_APP_ENABLE_TICTACTOE && <RegularNavItem label="Tic Tac Toe" />}
+        {process.env.REACT_APP_ENABLE_CONNECTFOUR && <RegularNavItem label="Connect Four" />}
+        {process.env.REACT_APP_ENABLE_BLACKJACK && <RegularNavItem label="Blackjack" />}
+        {process.env.REACT_APP_ENABLE_LIFE && <RegularNavItem label="Life" />}
+        {process.env.REACT_APP_ENABLE_AVILA && <RegularNavItem label="Avila" />}
+        {process.env.REACT_APP_AVILA_TILE_TEST && <RegularNavItem label="Tile Tester" route="/avila-tile-test" />}
+      </div>
+    </nav>
+  );
+};
+
+interface RegularNavLinkProps {
+  label: string;
+  route?: string;
+}
+
+const RegularNavItem: React.FC<RegularNavLinkProps> = ({ label, route }) => {
+  const path = route ?? `/${label.toLowerCase().replaceAll(" ", "-")}`;
+  const { pathname } = useLocation();
+  const isSelected = pathname.endsWith(path);
+  return (
+    <Link className={isSelected ? "selected" : ""} to={path}>
+      {label}
+    </Link>
+  );
 };
