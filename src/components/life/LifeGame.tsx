811cc0a5d5374d31eebc050e5d13d3e349faa29f
Jared Clark <rjaredclark@gmail.com>
2025-02-17 15:57:21 -0500

    fix timeout bug, reformat file

--- src/components/life/LifeGame.tsx
+++ src/components/life/LifeGame.tsx
@@ -7,59 +7,62 @@ import { LifeControls } from "./LifeControls";
 import { LifePresetControls } from "./LifePresetControls";
 
 export const LifeGame: React.FC<{}> = () => {
-    const shellRef = useRef<HTMLDivElement>(null);
+  const shellRef = useRef<HTMLDivElement>(null);
 
-    const dispatch = useAppDispatch();
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [advanceTrigger, setAdvanceTrigger] = useState(0);
-    const [interval, setInterval] = useState(375); // milliseconds
+  const dispatch = useAppDispatch();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [advanceTrigger, setAdvanceTrigger] = useState(0);
+  const [interval, setInterval] = useState(375); // milliseconds
 
-    const tripper = () => {
-        setAdvanceTrigger((val) => val + 1);
-        dispatch(advanceGeneration());
+  const tripper = () => {
+    setAdvanceTrigger((val) => val + 1);
+    dispatch(advanceGeneration());
+  };
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | null = null;
+    if (isPlaying) {
+      timeoutId = setTimeout(() => tripper(), interval);
+    }
+
+    return () => {
+      timeoutId && clearTimeout(timeoutId);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isPlaying, tripper, advanceTrigger]);
 
-    useEffect(() => {
-        if (isPlaying) {
-            setTimeout(() => tripper(), interval);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isPlaying, tripper, advanceTrigger]);
+  const trySetInterval = (newInterval: number) => {
+    if (!isNaN(newInterval) && newInterval >= 0) {
+      setInterval(newInterval);
+    }
+  };
 
-    const trySetInterval = (newInterval: number) => {
-        if (!isNaN(newInterval) && newInterval >= 0) {
-            setInterval(newInterval);
-        }
-    };
+  /** add a resize observer to the outermost div */
+  useEffect(() => {
+    if (shellRef && shellRef.current) {
+      const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+        const width = entries[0].contentBoxSize[0].inlineSize;
+        const height = entries[0].contentBoxSize[0].blockSize;
+        dispatch(boardResize({ width: width, height: height }));
+      });
 
-    /** add a resize observer to the outermost div */
-    useEffect(() => {
-        if (shellRef && shellRef.current) {
-            const observer = new ResizeObserver(
-                (entries: ResizeObserverEntry[]) => {
-                    const width = entries[0].contentBoxSize[0].inlineSize;
-                    const height = entries[0].contentBoxSize[0].blockSize;
-                    dispatch(boardResize({ width: width, height: height }));
-                }
-            );
+      observer.observe(shellRef.current);
 
-            observer.observe(shellRef.current);
+      return () => observer.disconnect();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shellRef]);
 
-            return () => observer.disconnect();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [shellRef]);
-
-    return (
-        <div className={styles.gameShell} ref={shellRef}>
-            <LifeControls
-                isPlaying={isPlaying}
-                setIsPlaying={setIsPlaying}
-                interval={interval}
-                trySetInterval={trySetInterval}
-            />
-            <LifePresetControls />
-            <LifeBoard />
-        </div>
-    );
+  return (
+    <div className={styles.gameShell} ref={shellRef}>
+      <LifeControls
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        interval={interval}
+        trySetInterval={trySetInterval}
+      />
+      <LifePresetControls />
+      <LifeBoard />
+    </div>
+  );
 };
